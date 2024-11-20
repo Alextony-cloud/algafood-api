715fc42480967efcec944477574bde267bdff5a8
Alextony-cloud <alextonyrodrigues@gmail.com>
2024-11-20 17:59:47 -0300

    Iniciando projeto algafood

---
+++ src/test/java/io/github/alextonycloud/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alextonycloud.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
