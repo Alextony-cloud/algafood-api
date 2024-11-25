541787d66bd655b622bd4c12c301afb6007b750b
Alextony-cloud <alextonyrodrigues@gmail.com>
2024-11-25 17:56:04 -0300

    testando o headers com httpstatus.FOUND

--- src/main/java/io/github/alextonycloud/algafood/api/controller/CozinhaController.java
+++ src/main/java/io/github/alextonycloud/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package io.github.alextonycloud.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.alextonycloud.algafood.api.model.CozinhasXmlWrapper;
@@ -27,7 +29,7 @@ public class CozinhaController {
 	@GetMapping()
 	public ResponseEntity<List<Cozinha>> listar() {
 		List <Cozinha> cozinhas = cozinhaRepository.listar();
-		return ResponseEntity.ok(cozinhas);
+		return ResponseEntity.ok().body(cozinhas);
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
@@ -38,6 +40,12 @@ public class CozinhaController {
 	@GetMapping("/{id}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
 		Cozinha cozinha = cozinhaRepository.buscar(id);
-		return ResponseEntity.ok(cozinha);
+		if(cozinha != null) {
+			return ResponseEntity.ok().body(cozinha);
+		}
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
 	}
 }
